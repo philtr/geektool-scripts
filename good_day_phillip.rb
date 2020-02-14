e2bb0817b02f97652a49dfef70d2b0ad57e048fc
Phillip Ridlen <p@rdln.net>
2020-02-14 12:47:53 -0600

    Initial commit

---
+++ good_day_phillip.rb
@@ -0,0 +1,12 @@
+case Time.now.hour
+when 4..11
+  puts "Good morning, Phillip."
+when 12..16
+  puts "Good afternoon, Phillip."
+when 17..21
+  puts "Good evening, Phillip."
+when 21..23
+  puts "Good night, Phillip."
+when 0..3
+  puts "Go to bed, Phillip."
+end
